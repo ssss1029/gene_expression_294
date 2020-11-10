8a19e8b967b4ea6f0ddf937b91dd3ad0a010ad99
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-11-09 20:45:07 -0800

    oops

--- models/DeepChrome_MNIST.py
+++ models/DeepChrome_MNIST.py
@@ -16,15 +16,15 @@ class DeepChromeMNISTModel(torch.nn.Module):
         super(DeepChromeMNISTModel, self).__init__()
         
         # TODO: If we want to change these, add command line args.
-        kernel_size = 4 # used to be 5
+        kernel_size = 5 # used to be 5
         num_filters = 50
         pool_size = 5 # used to be 5
-        mlp_h1 = 580 # used to be 625
+        mlp_h1 = 728 # used to be 625
         mlp_h2 = 125 # used to be 125
         noutputs = 10
 
         self.stage1 = nn.Sequential(
-            nn.Conv2d(1, num_filters, kernel_size=(kernel_size, 5)),
+            nn.Conv2d(1, num_filters, kernel_size=(kernel_size, kernel_size)),
             nn.ReLU(),
         )
 
@@ -34,7 +34,7 @@ class DeepChromeMNISTModel(torch.nn.Module):
 
         self.stage3 = nn.Sequential(
             nn.Dropout(0.5), # When changing this, check if this is the prob of keeping something or the prob of dropping it.
-            nn.Linear(1000, mlp_h1),
+            nn.Linear(800, mlp_h1),
             nn.ReLU(),
             nn.Linear(mlp_h1, mlp_h2),
             nn.ReLU(),
@@ -46,9 +46,6 @@ class DeepChromeMNISTModel(torch.nn.Module):
         Do a forward pass
         """
         batch_size = x.shape[0]
-
-        x = x.unsqueeze(1)
-        assert x.shape[1] == 1
 
         print(x.shape)
 
@@ -70,7 +67,7 @@ if __name__ == "__main__":
     # Do some sanity check
     model = DeepChromeMNISTModel().train()
     print(f"NUM PARAMETERS = {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
-    X = torch.ones((3, 28, 28)) # Example batch size of 3 
+    X = torch.ones((3, 1, 28, 28)) # Example batch size of 3 
     retval = model(X)
     print(retval)
     print(retval.shape)
