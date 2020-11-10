d66af39e3da606aa480e4c5464b5cd45ea47faf7
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-11-09 19:51:13 -0800

    mnist version

--- models/DeepChrome_MNIST.py
+++ models/DeepChrome_MNIST.py
@@ -7,18 +7,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class DeepChromeModel(torch.nn.Module):
+class DeepChromeMNISTModel(torch.nn.Module):
     def __init__(self):
         """
         DeepChrome model.
         Comments are shape descriptions. 
         """
-        super(DeepChromeModel, self).__init__()
+        super(DeepChromeMNISTModel, self).__init__()
         
         # TODO: If we want to change these, add command line args.
         kernel_size = 3 # used to be 5
         num_filters = 50
-        pool_size = 3
+        pool_size = 3 # used to be 5
         mlp_h1 = 1024 # used to be 625
         mlp_h2 = 256 # used to be 125
         noutputs = 10
@@ -50,17 +50,11 @@ class DeepChromeModel(torch.nn.Module):
         x = x.unsqueeze(1)
         assert x.shape[1] == 1
 
-        print(x.shape)
-
         x = self.stage1(x) 
         x = x.squeeze(3) 
 
-        print(x.shape)
-
         x = self.stage2(x)
         x = x.reshape((batch_size, -1))
-
-        print(x.shape)
 
         x = self.stage3(x)
         
@@ -68,7 +62,7 @@ class DeepChromeModel(torch.nn.Module):
 
 if __name__ == "__main__":
     # Do some sanity check
-    model = DeepChromeModel()
+    model = DeepChromeMNISTModel()
     X = torch.ones((3, 28, 28)) # Example batch size of 3 
     retval = model(X)
     print(retval)
